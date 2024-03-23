37e9f0787b249fa87d375d9aaab02c6402727715
Pavel Vaitsikhouski <pavel.vaitsikhouski@gmail.com>
2024-03-23 13:23:36 +0100

    Initial Commit

---
+++ DemoRC/DemoRCApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoRCApp.swift
+//  DemoRC
+//
+//  Created by Pavel Vaitsikhouski on 23.03.24.
+//
+
+import SwiftUI
+
+@main
+struct DemoRCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
